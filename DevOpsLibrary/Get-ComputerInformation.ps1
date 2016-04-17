3af0a7e8bc636d415dbc05d9fa20cc78bead6951
Ron Bowen <spam2lunsar@gmail.com>
2016-04-17 00:35:34 -0400

    Get-ComputerInformation
    Use this PS script to pull ComputerName, Manufacturur, Model, OS, OS
    Version, Serial Number from a machine.

---
+++ DevOpsLibrary/Get-ComputerInformation.ps1
@@ -0,0 +1,23 @@
+﻿Function Get-ComputerInformation
+{
+    PARAM ($ComputerName)
+    # Computer System
+    $ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem
+    # Operating System
+    $OperatingSystem = Get-WmiObject -Class Win32_OperatingSystem
+    # BIOS
+    $BIOS = Get-WmiObject -Class Win32_BIOS
+
+    # Prepare Output
+    $Properties = @{
+    ComputerName = $ComputerName
+    Manufacturer = $ComputerSystem.Manufacturer
+    Model = $ComputerName.Model
+    OperatingSystem = $OperatingSystem.Caption
+    OperatingSystemVersion = $OperatingSystem.Version
+    SerialNumber = $BIOS.SerialNumber
+    }
+
+    # Output INformation
+    New-Object -TypeName PSobject -Property $Properties
+    }
